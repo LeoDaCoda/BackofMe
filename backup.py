7f5daa903fdb1b42ee4b80d09fdc448c49633e74
LeoDaKoda <leodacoda@gmail.com>
2023-03-31 13:53:39 -0400

    BM-13 Created Git Wrapper class; currently support and tests serialize local and remote file system

--- backup.py
+++ backup.py
@@ -3,47 +3,43 @@ from git import Repo, NoSuchPathError
 
 
 class GitRapper:
-    def __init__(self):
-        self.root = 'test_file_sys'
-        self.repo = Repo(self.root)
+    def __init__(self, root='test_file_sys'):
+        self.root = root
+        if not os.path.exists(self.root):
+            raise FileNotFoundError
+        self.repo = Repo(self.root)  # will raise git.NoSuchPathError if given invalid path
+
+    def serialize_local_fs(self):
+        def serialize_recursive(sub_fs: dict, parent: list, curr_dirr: str):
+            # No need for base case b/c recursion will not be called if len(dirs) = 0
+
+            pwd = f'{"/".join(parent)}/{curr_dirr}' if len(parent) > 0 else curr_dirr
+            paths = os.listdir(pwd)
+            paths = [p for p in paths if p != ".git"]  # removes hidden folders
+            files = [f for f in paths if os.path.isfile(f"{pwd}/{f}")]
+            dirs = [d for d in paths if os.path.isdir(f"{pwd}/{d}")]
+
+            parent.append(curr_dirr)
+            sub_fs["files"] = files
+            sub_fs["directories"] = {d: serialize_recursive({}, list(parent), d) for d in dirs}  # no base case
+
+            return sub_fs
+
+        file_system = {}
+        return serialize_recursive(file_system, [], self.root)
+
+    def serialize_git_tree(self):
+        tree = self.repo.tree()
+
+        def serialize_recursive(sub_fs: dict, tree):
+            sub_fs["files"] = [f.name for f in tree if f.type == 'blob']
+            sub_fs["directories"] = {d.name: serialize_recursive({}, d) for d in tree if d.type == 'tree'}
+            return sub_fs
+
+        return serialize_recursive({}, tree)
 
 
-def serialize_local_fs(root_path: str):
-    # If path is not valid return an empty dictionary
-    try:
-        paths = os.listdir(root_path)
-    except FileNotFoundError:
-        return {}
-
-    def serialize_recursive(sub_fs: dict, parent: list, curr_dirr: str):
-        # No need for base case b/c recursion will not be called if len(dirs) = 0
-
-        pwd = f'{"/".join(parent)}/{curr_dirr}' if len(parent) > 0 else curr_dirr
-        paths = os.listdir(pwd)
-        paths = [p for p in paths if p != ".git"]  # removes hidden folders
-        files = [f for f in paths if os.path.isfile(f"{pwd}/{f}")]
-        dirs = [d for d in paths if os.path.isdir(f"{pwd}/{d}")]
-
-        parent.append(curr_dirr)
-        sub_fs["files"] = files
-        sub_fs["directories"] = {d: serialize_recursive({}, list(parent), d) for d in dirs}  # no base case
-
-        return sub_fs
-
-    file_system = {}
-    return serialize_recursive(file_system, [], root_path)
 
 
-def serialize_git_tree(root="test_file_sys"):
-    # to set up for test until class is implemented
-    try:
-        mrrepo = Repo(root)
-    except NoSuchPathError:
-        return {}
 
-    tree = mrrepo.tree()
-    def serialize_recursive(sub_fs: dict , tree):
-        sub_fs["files"] = [f.name for f in tree if f.type == 'blob']
-        sub_fs["directories"] = {d.name: serialize_recursive({}, d) for d in tree if d.type == 'tree'}
-        return sub_fs
-    return serialize_recursive({}, tree)
+
--- test.py
+++ test.py
@@ -1,40 +1,15 @@
 import unittest
-from backup import serialize_local_fs, serialize_git_tree
+
+import git
+from backup import GitRapper
 
 
 class MyTestCase(unittest.TestCase):
 
     def setUp(self):
         self.root = "test_file_sys"
-
-    def assert_nested_file_sys_equal(self, d1: dict, d2: dict):
-        d1_keys = list(d1.keys())
-        d2_keys = list(d2.keys())
-
-        self.assertCountEqual(d1_keys, d2_keys)
-        for k in d1_keys:
-            if isinstance(d1[k], list):
-                self.assertCountEqual(d1[k], d2[k])
-            else: # if not a list then a nested dictionary
-                self.assert_nested_file_sys_equal(d1[k], d2[k])
-    def test_assert_nested_dict_equal_pass(self):
-        x, y = ({"a": [1, 2, 3], "b": {"c": [4, 5, 6]}}, {"b": {"c": [6, 4, 5]}, "a": [3, 2, 1]})
-        self.assert_nested_file_sys_equal(x, y)
-
-    def test_assert_nested_dict_equal_fail(self):
-        x, y = ({"a": [1, 2, 3], "b": {"c": [13, 12]}}, {"b": {"c": [6, 4, 5]}, "a": [3, 2, 1]})
-        try:
-            self.assert_nested_file_sys_equal(x, y)  # Test passes if this assertion fails
-        except AssertionError:
-            return
-
-        self.assertTrue(False)  # if previous assertion passes this test FAILS
-
-    def test_path_dne(self):
-        self.assertEqual({}, serialize_local_fs("DNE"))
-
-    def test_serialized_file_system(self):
-        expected_output = {
+        self.repo_rapper = GitRapper(self.root)
+        self.expected_tree = {
             "files": ["file5.txt"],
             "directories": {
                 "Desktop": {
@@ -53,14 +28,42 @@ class MyTestCase(unittest.TestCase):
             }
         }
 
+    def test_invalid_root_path(self):
+        self.assertRaises(FileNotFoundError, GitRapper, "Path DNE")
+
+    def test_error_raised_invalid_repo_path(self):
+        self.assertRaises(git.InvalidGitRepositoryError, GitRapper, self.root+"/Desktop")
+
+    def assert_nested_file_sys_equal(self, d1: dict, d2: dict):
+        d1_keys = list(d1.keys())
+        d2_keys = list(d2.keys())
+
+        self.assertCountEqual(d1_keys, d2_keys)
+        for k in d1_keys:
+            if isinstance(d1[k], list):
+                self.assertCountEqual(d1[k], d2[k])
+            else: # if not a list then a nested dictionary
+                self.assert_nested_file_sys_equal(d1[k], d2[k])
+
+    def test_assert_nested_dict_equal_pass(self):
+        x, y = ({"a": [1, 2, 3], "b": {"c": [4, 5, 6]}}, {"b": {"c": [6, 4, 5]}, "a": [3, 2, 1]})
+        self.assert_nested_file_sys_equal(x, y)
+
+    def test_assert_nested_dict_equal_fail(self):
+        x, y = ({"a": [1, 2, 3], "b": {"c": [13, 12]}}, {"b": {"c": [6, 4, 5]}, "a": [3, 2, 1]})
+        try:
+            self.assert_nested_file_sys_equal(x, y)  # Test passes if this assertion fails
+        except AssertionError:
+            return
+
+        self.assertTrue(False)  # if previous assertion passes this test FAILS
+
+    def test_serialized_file_system(self):
         # expected_output = (["file5.txt"], ["Desktop", "Downloads"])
-        self.assert_nested_file_sys_equal(expected_output, serialize_local_fs(self.root))
+        self.assert_nested_file_sys_equal(self.expected_tree, self.repo_rapper.serialize_local_fs())
 
     def test_serialized_git_tree(self):
-        self.assert_nested_file_sys_equal(serialize_local_fs(self.root), serialize_git_tree())
-
-    def test_serialized_git_tree_invalid_path(self):
-        self.assertEqual({}, serialize_git_tree("DNE"))
+        self.assert_nested_file_sys_equal(self.expected_tree, self.repo_rapper.serialize_git_tree())
 
 
 if __name__ == '__main__':
